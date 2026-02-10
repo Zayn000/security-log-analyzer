80c83a1cd099b707fdeea12b6f2006fa9406a711
Zayn Hasan <135254239+Zayn000@users.noreply.github.com>
2026-02-10 13:44:09 +0300

    Update test-run.py

--- tests/test-run.py
+++ tests/test-run.py
@@ -1,7 +1,9 @@
 from src.parser import parse_auth_log
 from src.analyzer import analyze_auth_activity
+from src.detector import detect_suspicious_ips
 
 df = parse_auth_log("logs/sample_auth.log")
 summary = analyze_auth_activity(df)
+suspicious = detect_suspicious_ips(summary)
 
-print(summary)
+print(suspicious)
